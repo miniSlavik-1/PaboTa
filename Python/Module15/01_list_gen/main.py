e6b2524355b23f98b1e9f196eb488bf3101a0c03
Vladimir <pogromskyvladimir@yandex.ru>
2024-09-18 20:14:48 +0300

    task 14.1 completed

--- Python/Module15/01_list_gen/main.py
+++ Python/Module15/01_list_gen/main.py
@@ -1,1 +1,6 @@
-#TODO Код писать здесь!
+number = int(input("Введите число: "))
+res = []
+for n in range(1, number + 1, 2):
+    res.append(n)
+
+print("Список из нечётных чисел от одного до N: ", res)
