5994676f6592b5db73c07a4e020ecf74b0cf62e9
Vladimir <pogromskyvladimir@yandex.ru>
2024-09-18 20:37:48 +0300

    task 15.3 completed

--- Python/Module15/03_videocards/main.py
+++ Python/Module15/03_videocards/main.py
@@ -1,1 +1,11 @@
-#TODO Код писать здесь!
+num = int(input("Количество видеокарт: "))
+nvidia = []
+for n in range(num):
+    nvidia.append(int(input(str(n + 1) + " Видеокарта: ")))
+
+print("Старый список видеокарт:", nvidia)
+newvidia = []
+for elem in nvidia:
+    if elem < max(nvidia):
+        newvidia.append(elem)
+print("Новый список видеокарт:", newvidia)
