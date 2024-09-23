06bb6804496d3be6a17d10df00047b99a22a929d
Vladimir <pogromskyvladimir@yandex.ru>
2024-09-23 12:51:30 +0300

    task 15.6 completed

--- Python/Module15/06_running_nums/main.py
+++ Python/Module15/06_running_nums/main.py
@@ -1,1 +1,7 @@
-#TODO Код писать здесь!
+sdvig = int(input("Сдвиг: "))
+tablo = [1, 2, 3, 4, 5]
+print("Изначальный список:", tablo)
+for i in range(sdvig):
+    tablo.append(tablo[0])
+    tablo.pop(0)
+print("Сдвинутый список:", tablo)
