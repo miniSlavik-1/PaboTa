7d3f529cb5ecd469699e31d2369abeae9c9726ea
Vladimir <pogromskyvladimir@yandex.ru>
2024-09-23 13:27:13 +0300

    task 15.9 не совсем completed

--- Python/Module15/09_reverse_analysis/main.py
+++ Python/Module15/09_reverse_analysis/main.py
@@ -1,5 +1,16 @@
-
-# Список чисел для работы (итоговый алгоритм проверьте на разных списках, придуманных самостоятельно):
+# Я честно говоря не очень понял, в чем смысл задачи и почему программа решила что 9 - четное число
 numbers_list = [7, 14, 3, 18, 21, 10, 9, 6]
+for i in range(len(numbers_list)):
+    t = 0
+    for j in range(0, len(numbers_list) - 1):
+        if numbers_list[j + 1] < numbers_list[j]:
+            t = numbers_list[j + 1]
+            numbers_list[j + 1] = numbers_list[j]
+            numbers_list[j] = t
+            t = 0
 
-#TODO Код писать здесь!
+for k in numbers_list:
+    if k % 2 == 1:
+        numbers_list.remove(k)
+
+print(numbers_list)
