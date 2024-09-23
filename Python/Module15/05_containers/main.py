a45faeb738f18358dd2790aa91abc810f7402a69
Vladimir <pogromskyvladimir@yandex.ru>
2024-09-23 12:45:04 +0300

    task 15.5 completed

--- Python/Module15/05_containers/main.py
+++ Python/Module15/05_containers/main.py
@@ -1,1 +1,12 @@
-#TODO Код писать здесь!
+num = int(input("Количество контейнеров: "))
+containers = []
+for n in range(num):
+    cont = int(input("Введите вес контейнера: "))
+    containers.append(cont)
+
+x = int(input("Введите вес нового контейнера: "))
+c = 1
+for i in containers:
+    if x <= i:
+        c += 1
+print("Номер, который получит новый контейнер:", c)
