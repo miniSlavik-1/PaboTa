ef5d248d7c26181351f2f424a2391e4f95f43f44
Vladimir <pogromskyvladimir@yandex.ru>
2024-09-30 13:24:58 +0300

    16.7 не совсем completed

--- Python/Module16/07_rhyme_cnt/main.py
+++ Python/Module16/07_rhyme_cnt/main.py
@@ -1,1 +1,15 @@
-#TODO Код писать здесь
+mans = [x+1 for x in range(int(input("Кол-во человек: ")))]
+death_num = int(input("Какое число в считалке? "))
+print("Значит, выбывает каждый", str(death_num) + "-й человек")
+
+print("Текущий круг людей:", mans)
+print("Начало счёта с номера", 1)
+
+while len(mans) > 1:
+    print("Выбывает человек под номером", mans[death_num % len(mans) - 1])
+    mans.pop(death_num % len(mans) - 1)
+    print()
+    print("Текущий круг людей:", mans)
+    print("Начало счёта с номера", death_num%len(mans) + 1)
+
+print("Остался человек под номером", mans)
