338a19eda56b544c5fab854c4730ee5977b4334d
Vladimir <pogromskyvladimir@yandex.ru>
2024-09-30 13:33:22 +0300

    16.8 не completed

--- Python/Module16/08_simmetrical_seq/main.py
+++ Python/Module16/08_simmetrical_seq/main.py
@@ -1,1 +1,10 @@
-#TODO Код писать здесь
+num = int(input("Кол-во чисел: "))
+nummers = []
+for i in range(num):
+    nummers.append(int(input("Число: ")))
+
+if nummers == nummers.reverse():
+    print("Последовательность симметрична")
+
+else:
+    pass
