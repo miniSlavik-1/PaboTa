098b8a36ef525e65acb092d27366fe452b99cf05
Vladimir <pogromskyvladimir@yandex.ru>
2024-09-30 12:45:20 +0300

    16.4 completed

--- Python/Module16/04_party/main.py
+++ Python/Module16/04_party/main.py
@@ -1,3 +1,17 @@
 guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
 
-#TODO Код писать здесь
+was = ""
+while was != "Пора спать":
+    print("Сейчас на вечеринке", len(guests), "гостей:", guests)
+    was = input("Гость пришёл или ушёл: ")
+    if was == "пришёл":
+        wer = input("Имя гостя: ")
+        guests.append(wer)
+    elif was == "ушёл":
+        wer = input("Имя гостя: ")
+        guests.remove(wer)
+
+if len(guests) <= 6:
+    print("Вечеринка закончилась, все легли спать.")
+else:
+    print("Отдохнуть сейчас не получится: рядом бродят монстры")
