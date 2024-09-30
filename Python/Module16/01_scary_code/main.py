f3b3a7edb5c3a65a9ea485351be6efbf6fc95848
Vladimir <pogromskyvladimir@yandex.ru>
2024-09-30 12:10:43 +0300

    16.1 completed

--- Python/Module16/01_scary_code/main.py
+++ Python/Module16/01_scary_code/main.py
@@ -1,1 +1,13 @@
-#TODO Код писать здесь
+a = [1, 5, 3]
+b = [1, 5, 1, 5]
+c = [1, 3, 1, 5, 3, 3]
+a += b
+t1 = a.count(5)
+for i in range(t1):
+    a.remove(5)
+a += c
+t2 = a.count(3)
+
+print(t1)
+print(t2)
+print(a)
