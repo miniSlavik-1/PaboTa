e42d0f518b5b4436acaf266cdd55dc9f0c8712ba
Vladimir <pogromskyvladimir@yandex.ru>
2024-09-30 12:16:44 +0300

    16.2 completed

--- Python/Module16/02_unique_combination/main.py
+++ Python/Module16/02_unique_combination/main.py
@@ -1,1 +1,11 @@
-#TODO Код писать здесь
+list1 = [1, 3, 5, 7, 9]
+list2 = [2, 4, 5, 6, 8, 10]
+
+lisst = list1 + list2
+lisst.sort()
+for i in lisst:
+    t = lisst.count(i)
+    for j in range(t - 1):
+        lisst.remove(i)
+
+print(lisst)
