d1b63a766e3cd48f99d327deb467b6079877924c
Vladimir <pogromskyvladimir@yandex.ru>
2024-11-10 16:32:08 +0300

    test 17.3 completed

--- Python/Module17/03_random_competition/main.py
+++ Python/Module17/03_random_competition/main.py
@@ -1,1 +1,17 @@
-#TODO Написать код
+import random
+
+def sorevn(a, b):
+    c = []
+    for i in range(20):
+        if a[i] > b[i]:
+            c.append(a[i])
+        elif a[i] < b[i]:
+            c.append(b[i])
+    return(c)
+
+a = [random.randint(5, 10) for x in range(20)]
+b = [random.randint(5, 10) for x in range(20)]
+
+print("Первая команда:", a)
+print("Вторая команда:", b)
+print("Победители тура:", sorevn(a, b))
