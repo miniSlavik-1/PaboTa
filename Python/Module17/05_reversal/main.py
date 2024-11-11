166fa95b06cd0ceb53e8f24a63d3732e4777e0a8
Vladimir <pogromskyvladimir@yandex.ru>
2024-11-11 12:36:38 +0300

    test 17.4 completed

--- Python/Module17/05_reversal/main.py
+++ Python/Module17/05_reversal/main.py
@@ -1,1 +1,11 @@
-#TODO Написать код
+def perevorot(strr: str):
+    hi = []
+    for i in range(len(strr)):
+        if strr[i] == "h":
+            hi.append(i)
+    hi = sorted(hi)
+    print("Развёрнутая последовательность между первым и последним h:", strr[max(hi): min(hi): -1])
+
+
+strok = input("Введите строку: ")
+perevorot(strok)
