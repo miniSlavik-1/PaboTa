5a3b01b2db7a6f41744ed75bc36bcf8a8d3131c5
Vladimir <pogromskyvladimir@yandex.ru>
2024-11-11 12:54:23 +0300

    test 17.7 completed

--- Python/Module17/07_list_of_lists_2/main.py
+++ Python/Module17/07_list_of_lists_2/main.py
@@ -1,1 +1,12 @@
-#TODO Написать код
+def conserva(spis):
+    new_spis = []
+    for i in range(len(spis)):
+        for j in range(len(spis[i])):
+            for z in range(len(spis[i][j])):
+                copy = spis[i][j][z]
+                new_spis.append(copy)
+    return new_spis
+
+
+nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
+print("Ответ:", conserva(nice_list))
