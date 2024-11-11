e9599c2cf76278537f545ca556f9e5467205c620
Vladimir <pogromskyvladimir@yandex.ru>
2024-11-11 12:41:13 +0300

    test 17.6 completed

--- Python/Module17/06_two_dimensional_list/main.py
+++ Python/Module17/06_two_dimensional_list/main.py
@@ -1,1 +1,9 @@
-#TODO Написать код
+import random
+
+list1 = []
+for i in range(4):
+    list2 = []
+    for j in range(3):
+        list2.append(random.randint(0, 20))
+    list1.append(list2)
+print(list1)
