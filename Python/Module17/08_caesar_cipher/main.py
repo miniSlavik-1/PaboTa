5aad274ceadf0646906b4ef66eb873dc40f87821
Vladimir <pogromskyvladimir@yandex.ru>
2024-11-11 13:14:42 +0300

    test 17.8 completed

--- Python/Module17/08_caesar_cipher/main.py
+++ Python/Module17/08_caesar_cipher/main.py
@@ -1,1 +1,15 @@
-#TODO Написать код
+def shifer(stroka: str, key: int):
+    alp = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
+    new_stroka = ""
+    for i in range(len(stroka)):
+        if stroka[i] in alp:
+            ind = (alp.index(stroka[i]) + key) % len(alp)
+            new_stroka += alp[ind]
+        else:
+            new_stroka += stroka[i]
+    return new_stroka
+
+
+strok = input("Введите сообщение: ")
+key = int(input("Введите сдвиг: "))
+print("Зашифрованное сообщение:", shifer(strok, key))
