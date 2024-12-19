11253137697d9021a2167017e0c8d483e30c219e
Vladimir <pogromskyvladimir@yandex.ru>
2024-12-19 14:23:34 +0300

    m 19.5 completed

--- Python/Module19/05_synonym_dict/main.py
+++ Python/Module19/05_synonym_dict/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+def sinonim(n):
+    slovo = {}
+    for i in range(n):
+        print(i + 1, "пара:")
+        a = input("")
+        b = input("")
+        slovo[a] = b
+        slovo[b] = a
+    return slovo
+
+def search(slovo, text):
+    if text in slovo:
+        return "Синоним:", slovo[text]
+    else:
+        return "Такого слова в словаре нет."
+
+n = int(input("Введите количество пар слов: "))
+text = input("Введите слово: ")
+print(search(sinonim(n), text))
