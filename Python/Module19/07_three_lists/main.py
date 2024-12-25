8b1202f04249f5ece27d7e95b73a80fd8b15dd5b
Vladimir <pogromskyvladimir@yandex.ru>
2024-12-25 14:34:46 +0300

    m 19 +- completed

--- Python/Module19/07_three_lists/main.py
+++ Python/Module19/07_three_lists/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+array_1 = [1, 5, 10, 20, 40, 80, 100]
+array_2 = [6, 7, 20, 80, 100]
+array_3 = [3, 4, 15, 20, 30, 70, 80, 120]
+new_mass = []
+new_mass_1 = []
+for i in array_1:
+    if i in array_2:
+        if i in array_3:
+            new_mass.append(i)
+    if i not in array_2:
+        if i not in array_3:
+            new_mass_1.append(i)
+
+print(new_mass)
+print(new_mass_1)
--- Python/Module19/08_palindrome_again/main.py
+++ Python/Module19/08_palindrome_again/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+a = input("Введите строку: ")
+line = 0
+for i in range(len(a)):
+    if a.count(a[i]) % 2 != 0:
+        line += 1
+if line > 1:
+    print("Нельзя сделать палиндромом")
+else:
+    print("Можно сделать палиндромом")
